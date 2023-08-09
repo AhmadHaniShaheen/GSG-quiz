d77149b184844807130f665c4981d5ce2bd7176f
ahmadhanialishaheen@gmail.com <ahmadhanialishaheen@gmail.com>
2023-08-09 15:38:21 +0300

    capture & share image

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,5 +1,6 @@
 import 'package:api_qize/models/image.dart';
 import 'package:api_qize/models/qoute.dart';
+import 'package:api_qize/service/capture_image.dart';
 import 'package:api_qize/service/image_api.dart';
 import 'package:api_qize/service/qoute.dart';
 import 'package:flutter/material.dart';
@@ -15,6 +16,7 @@ class _HomePageState extends State<HomePage> {
   Qoute qoute = Qoute();
   late Future<Album> futureAlbum;
   late Future<ImageApi> futureImage;
+  GlobalKey globalKey = GlobalKey();
 
   @override
   void initState() {
@@ -27,104 +29,111 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {},
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () async {
+          await capturePng(context, globalKey);
+        },
+        label: const Text('Take screenshot'),
+        icon: const Icon(Icons.share_rounded),
       ),
       body: FutureBuilder(
         future: futureImage,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return Container(
-              height: double.infinity,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(
-                    snapshot.data!.url,
+            return RepaintBoundary(
+              key: globalKey,
+              child: Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      snapshot.data!.url,
+                    ),
+                    fit: BoxFit.cover,
                   ),
-                  fit: BoxFit.cover,
                 ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: FutureBuilder(
-                  future: futureAlbum,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      return Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            padding: const EdgeInsets.all(8),
-                            decoration: BoxDecoration(
-                              color: Colors.transparent.withOpacity(0.1),
-                              borderRadius: BorderRadius.circular(10),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black.withOpacity(0.2),
-                                  blurRadius: 9.0,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: FutureBuilder(
+                    future: futureAlbum,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                color: Colors.transparent.withOpacity(0.1),
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.2),
+                                    blurRadius: 9.0,
+                                  ),
+                                ],
+                              ),
+                              child: Text(
+                                snapshot.data!.contant,
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 32,
                                 ),
-                              ],
+                              ),
                             ),
-                            child: Text(
-                              snapshot.data!.contant,
-                              textAlign: TextAlign.center,
+                            Text(
+                              snapshot.data!.author,
                               style: const TextStyle(
                                 color: Colors.white,
-                                fontSize: 32,
+                                fontSize: 16,
                               ),
                             ),
-                          ),
-                          Text(
-                            snapshot.data!.author,
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
+                            const SizedBox(
+                              height: 50,
                             ),
-                          ),
-                          const SizedBox(
-                            height: 50,
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(8),
-                            width: 60,
-                            height: 60,
-                            decoration: BoxDecoration(
-                              color: const Color.fromARGB(255, 235, 154, 208)
-                                  .withOpacity(0.5),
-                              borderRadius: BorderRadius.circular(10),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black.withOpacity(0.2),
-                                  blurRadius: 9.0,
+                            Container(
+                              padding: const EdgeInsets.all(8),
+                              width: 60,
+                              height: 60,
+                              decoration: BoxDecoration(
+                                color: const Color.fromARGB(255, 235, 154, 208)
+                                    .withOpacity(0.5),
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.2),
+                                    blurRadius: 9.0,
+                                  ),
+                                ],
+                              ),
+                              child: InkWell(
+                                onTap: () {
+                                  setState(() {
+                                    futureAlbum = Future.delayed(
+                                        const Duration(seconds: 1),
+                                        () => Qoute().fetchAlbum());
+                                    futureImage = Future.delayed(
+                                        const Duration(seconds: 1),
+                                        () => ImageProv().fetchImage());
+                                  });
+                                },
+                                child: const Icon(
+                                  Icons.arrow_forward_ios,
+                                  color: Colors.white,
                                 ),
-                              ],
-                            ),
-                            child: InkWell(
-                              onTap: () {
-                                setState(() {
-                                  futureAlbum = Future.delayed(
-                                      const Duration(seconds: 1),
-                                      () => Qoute().fetchAlbum());
-                                  futureImage = Future.delayed(
-                                      const Duration(seconds: 1),
-                                      () => ImageProv().fetchImage());
-                                });
-                              },
-                              child: const Icon(
-                                Icons.arrow_forward_ios,
-                                color: Colors.white,
                               ),
                             ),
-                          ),
-                        ],
-                      );
-                    } else if (snapshot.hasError) {
-                      return Center(child: Text('${snapshot.error}'));
-                    }
-                    return const Center(child: CircularProgressIndicator());
-                  },
+                          ],
+                        );
+                      } else if (snapshot.hasError) {
+                        return Center(child: Text('${snapshot.error}'));
+                      }
+                      return const Center(child: CircularProgressIndicator());
+                    },
+                  ),
                 ),
               ),
             );
